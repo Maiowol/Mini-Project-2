dc5dd10b9f0b0659c6ccd22037c81dc26a32b568
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-14 09:43:16 +0900

    상세 페이지 작업중

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,7 @@
+//상세 페이지
 import React from 'react'
 import styled from 'styled-components';
-
-
+import Avatar from '@mui/material/Avatar';
 
 //components
 import Header2 from '../components/Header2';
@@ -12,27 +12,44 @@ function Detail() {
             <Header2 />
             <DetailBox>
                 <DetailBlock>
-                    <Img src='' />
-                    <PostBox>
-                        {/* 유저닉네임, 제품명, 작성한 내용 */}
-                        <div className='nickname'></div>
-                        <div className='product'></div>
-                        <div className='content'></div>
-                        <button>글 삭제</button>
-                    </PostBox>
+
+                    {/* 유저닉네임, 제품명, 작성한 내용 */}
+                    <PhotoBox>
+                        <strong>nickname</strong>
+
+                        <div className='profile'>
+                            <img src=
+                                'https://newsimg.sedaily.com/2021/02/01/22ID302VAD_16.jpg' />
+                        </div>
+
+                        <div className='product'>제품명:
+                            <div> </div></div>
+                        <div className='content'>자세한 내용:
+                            <div> </div></div>
+
+                    </PhotoBox>
+
+                    {/* 댓글, 댓글남기기 */}
                     <CommentsBox>
-                       {/* 댓글, 댓글남기기 */}
-                       <strong>comments</strong>
-
+                        <strong>comments</strong>
+                        <div className='comments'>
+                            <div className='profile'>
+                                <Avatar
+                                    className='post__avatar'
+                                    alt='U'
+                                    src='/static/images/avatar/1.jpg'
+                                />
+                                <text>아이디값</text>
+                            </div>
+                        </div>
                     </CommentsBox>
-
-
                 </DetailBlock>
             </DetailBox>
 
         </>
     )
 }
+
 
 const DetailBox = styled.div`
 width: 850px;
@@ -62,7 +79,6 @@ width:80px;
 
 img {
 width: 300px;
-margin-top: 20px;
 
 }
 
@@ -85,26 +101,50 @@ margin-top: 10px;
 }
 `;
 
-const Img = styled.div`
-
-`;
 
 const PostBox = styled.div`
-
 display: flex;
 flex-direction: column;
-justify-content: space-between;
+justify-content: flex-end;
 border: 1px solid lightgray;
 padding: 0 1em;
 margin: 1em;
 width: 400px;
 height: 500px;
+
+strong {
+    display:flex;
+    flex-direction: row;
+    margin-top: 20px;
+    margin-left: 50px;
+    }
 `;
+
+
+const PhotoBox = styled.div`
+display: flex;
+flex-direction: column;
+border: 1px solid lightgray;
+padding: 0 1em;
+margin: 1em;
+width: 300px;
+height: 500px;
+
+strong {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.product {
+ margin-top: 10px;
+ margin-bottom: 40px;
+}
+`;
+
 
 const CommentsBox = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: space-between;
 border: 1px solid lightgray;
 padding: 0 1em;
 margin: 1em;
@@ -113,8 +153,31 @@ height: 500px;
 
 strong{
     margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.comments {
+display: flex;
+flex-direction: row;
+border: 1px solid #dee2e6;
+ height: 300px;;
+ padding-top: 10px;
+ padding-bottom: 20px;
+ padding-left: 7px;
+ overflow-y: auto; 
+
+.profile {
+display:flex;
+flex-direction: row;
+height: 50px;
+padding-top: 10px;
+padding-left: 7px;
+overflow-y: auto; 
+
+text {
+padding-top: 10px;
+padding-left: 7px;
 }
 `;
-
 
 export default Detail;

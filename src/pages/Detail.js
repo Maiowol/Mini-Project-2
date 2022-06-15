b4bbe2011098d342d3fdd4d9ea80b34d0a62b326
4hsnim <102935156+4hsnim@users.noreply.github.com>
2022-06-16 01:19:23 +0900

    중간병합!!
    병합해봅시다!

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,63 +1,63 @@
-//상세 페이지
 import React from 'react'
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from "axios"
 import Avatar from '@mui/material/Avatar';
+import { useSelector, useDispatch } from "react-redux";
+
+
+
 
 //components
 import Header2 from '../components/Header2';
+import DetailBox from '../components/DetailBox';
 
 function Detail() {
+    let {postId} = useParams()
+    const [state, setState] = React.useState("")
+
+    const dispatch = useDispatch();
+    const token = localStorage.getItem("token")
+
+    const [product, setProduct] = React.useState(['애플','삼성','LG']);
+    const [content, setContent] = React.useState(['Apple','Samsung','LG']);
+    // console.log(state)
+
+
+    React.useEffect( () => {
+         axios
+          .get("http://dlckdals04.shop/"+postId) 
+          .then((response) => {
+            setState(response.data);
+            console.log(response.data);
+          })
+          .catch((response) => {
+            console.log(response);
+          });
+      }, []);
+    
+    console.log(state)
+
+
     return (
         <>
             <Header2 />
-            <DetailBox>
+            <DetailPage>
                 <DetailBlock>
-
-                    {/* 유저닉네임, 제품명, 작성한 내용 */}
-                    <PhotoBox>
-                        <strong>nickname</strong>
-
-                        <div className='profile'>
-                            <img src=
-                                'https://newsimg.sedaily.com/2021/02/01/22ID302VAD_16.jpg' />
-                        </div>
-
-                        <div className='product'>제품명:
-                            <div> </div></div>
-                        <div className='content'>자세한 내용:
-                            <div> </div></div>
-
-                    </PhotoBox>
-
-                    {/* 댓글, 댓글남기기 */}
-                    <CommentsBox>
-                        <strong>comments</strong>
-                        <div className='comments'>
-                            <div className='profile'>
-                                <Avatar
-                                    className='post__avatar'
-                                    alt='U'
-                                    src='/static/images/avatar/1.jpg'
-                                />
-                                <text>아이디값</text>
-                            </div>
-                        </div>
-                    </CommentsBox>
+                 <DetailBox state={state}
+                 />  
                 </DetailBlock>
-            </DetailBox>
-
+            </DetailPage>
         </>
     )
+    
 }
 
-
-const DetailBox = styled.div`
+const DetailPage = styled.div`
 width: 850px;
 height: 600px;
-
 background: white;
 box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.09);
-
 margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
 margin-top: 50px;
 margin-bottom: 32px;
@@ -69,24 +69,19 @@ const DetailBlock = styled.div`
 margin: auto;
   display: flex;
   align-items: center;
-
 textarea {
 &:hover { 
 outline: 0.5px solid black;
 }
 width:80px;
 }
-
 img {
 width: 300px;
-
 }
-
 .picture {
 width: 300px;
 height: 200px;
 }
-
 textarea {
 width: 40%;
 height: 50%;
@@ -95,88 +90,8 @@ resize: none;
 margin: 0 auto;
 outline: none;
 }
-
 button {
 margin-top: 10px;   
-}
-`;
-
-
-const PostBox = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: flex-end;
-border: 1px solid lightgray;
-padding: 0 1em;
-margin: 1em;
-width: 400px;
-height: 500px;
-
-strong {
-    display:flex;
-    flex-direction: row;
-    margin-top: 20px;
-    margin-left: 50px;
-    }
-`;
-
-
-const PhotoBox = styled.div`
-display: flex;
-flex-direction: column;
-border: 1px solid lightgray;
-padding: 0 1em;
-margin: 1em;
-width: 300px;
-height: 500px;
-
-strong {
-    margin-top: 10px;
-    margin-bottom: 10px;
-}
-
-.product {
- margin-top: 10px;
- margin-bottom: 40px;
-}
-`;
-
-
-const CommentsBox = styled.div`
-display: flex;
-flex-direction: column;
-border: 1px solid lightgray;
-padding: 0 1em;
-margin: 1em;
-width: 300px;
-height: 500px;
-
-strong{
-    margin-top: 10px;
-    margin-bottom: 10px;
-}
-
-.comments {
-display: flex;
-flex-direction: row;
-border: 1px solid #dee2e6;
- height: 300px;;
- padding-top: 10px;
- padding-bottom: 20px;
- padding-left: 7px;
- overflow-y: auto; 
-
-.profile {
-display:flex;
-flex-direction: row;
-height: 50px;
-padding-top: 10px;
-padding-left: 7px;
-overflow-y: auto; 
-
-text {
-padding-top: 10px;
-padding-left: 7px;
 }
 `;
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,6 +5,7 @@ import axios from "axios"
 import Header from '../components/Header';
 import { useNavigate } from "react-router-dom";
 
+
 function SignUp() {
     const navigate = useNavigate();
     const id_ref = React.useRef(null)
@@ -16,6 +17,60 @@ function SignUp() {
     const [name, setName] = React.useState("");
     const [pw, setPw] = React.useState("");
     const [checkPw, setCheckPw] = React.useState("");
+
+
+    // //회원가입 유효성 검사
+    const checkID = () => {
+        const id_value = Array.from(id_ref.current.value)
+        if(id_value == "") {
+            alert('아이디를 입력해주세요!')
+            document.getElementById('id').focus()
+        } else if (id_value.length < 4) {
+            alert('4자리 이상 12자리 이하의 아이디를 적어주세요.')
+            document.getElementById('id').focus()
+         } else if (id_value.length > 12) {
+          alert('4자리 이상 12자리 이하의 아이디를 적어주세요.')
+         } else {
+            checkName()
+        }
+    };
+
+    const checkName = () => {
+        const name_value = Array.from(name_ref.current.value)
+        if(name_value == "") {
+            alert('닉네임을 입력해주세요!')
+            document.getElementById('name').focus()
+        } else if (name_value.length < 2) {
+            alert('2자리 이상 12자리 이하의 아이디를 적어주세요.')
+            document.getElementById('name').focus()
+        } else if (name_value.length > 12) {
+            alert('2자리 이상 12자리 이하의 아이디를 적어주세요.')
+            document.getElementById('name').focus()
+        } else {
+            checkPW()
+        }
+    };
+
+
+    const checkPW = () => {
+        const pw_value = Array.from(pw_ref.current.value)
+        if (pw_value== "") {
+            alert('비밀번호를 입력해주세요.')
+        } else if (pw_value.length < 4) {
+          alert('4자리 이상 12자리 이하의 비밀번호를 적어주세요.')
+        } else if (pw_value.length > 12){
+            alert('4자리 이상 12자리 이하의 비밀번호를 적어주세요.')
+            document.getElementById('pw').focus()
+        } else if(pwcheck_ref.current.value == ""){
+            alert('비밀번호를 한 번 더 체크해주세요.')
+            document.getElementById('checkPw').focus()
+        } else if(pw_ref.current.value !== pwcheck_ref.current.value) {
+            alert('비밀번호가 일치하지 않습니다.')
+            document.getElementById('checkPw').focus()
+        } else {
+            callSomethingAxios()
+        }   
+    };
 
 
     const callSomethingAxios =  () => {                    

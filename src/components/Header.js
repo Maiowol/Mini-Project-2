3ad4c429d293dc3cb8c8a4232b6210656ac61b13
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-15 19:48:14 +0900

    유효성 검사 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,37 +4,77 @@ import styled from 'styled-components';
 import Modal from 'react-modal'
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import axios from 'axios';
-
+import {useDispatch, useSelector} from "react-redux"
+import { actionCreators as userActions } from "../redux/modules/user"
 import Button from '@mui/material/Button';
-import { AiFillApple } from "react-icons/ai";
+import axios from "axios"
+
+
+import { useSelect } from '@mui/base';
+
 
 function Header(props) {
   const navigate = useNavigate();
+
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+  const dispatch = useDispatch()
 
   const id_ref = React.useRef(null);
   const pw_ref = React.useRef(null);
 
+  const login = () => {
+        dispatch(userActions.loginDB(id, pwd));
+        navigate('/');
+      };
 
-  // const JWT_EXPIRY_TIME = 24 * 3600 * 1000; // 만료 시간 (24시간 밀리 초로 표현)
+      const checkID = () => {
+        const id_value = Array.from(id_ref.current.value)
+        if(id_value == "") {
+            alert('아이디를 입력해주세요!')
+            document.getElementById('id').focus()
+        } else if (id_value.length < 4) {
+            alert('4자리 이상 12자리 이하의 아이디를 적어주세요.')
+            document.getElementById('id').focus()
+         } else if (id_value.length > 12) {
+          alert('4자리 이상 12자리 이하의 아이디를 적어주세요.')
+         } else {
+            checkPW()
+        }
+    };
+           
+    const checkPW = () => {
+      const pw_value = Array.from(pw_ref.current.value)
+      if (pw_value== "") {
+          alert('비밀번호를 입력해주세요.')
+      } else if (pw_value.length < 4) {
+        alert('4자리 이상 12자리 이하의 비밀번호를 적어주세요.')
+      } else if (pw_value.length > 12){
+          alert('4자리 이상 12자리 이하의 비밀번호를 적어주세요.')
+          document.getElementById('pw').focus()
+      } else { 
+          login()
+      }
+  };
 
   const callSomethingAxios = () => {
-    axios.post('http://dlckdals04.shop/login', {
-      "ID": id_ref.current.value,
-      "password": pw_ref.current.value
-    }, )
-          .then(function (response) {
-      alert("로그인이 완료되었습니다!")
-      navigate('/');
-      localStorage.setItem('access_token', response.data.token);
-      console.log(response)
-
-    }).catch(function (error) {
-      alert('로그인에 실패했습니다!')
+    axios.post("http://dlckdals04.shop/login", {
+        "ID": id_ref.current.value,
+        "password": pw_ref.current.value,
+       
+    }).then(function (response) {
+        alert("로그인 완료!")
+        localStorage.setItem("token", response.data.token);
+        setModalIsOpen(false)
+        console.log(response)
     })
-  };  
-  
+        .catch(function (error) {
+            alert("로그인을 다시해주세요")
+            console.log(error);
+        })
+}
+
   return (
     <>
     <NavBar>
@@ -48,8 +88,7 @@ function Header(props) {
             <li><a>others</a></li>
           </ul> */}
           <div className='button'>
-          {/* <button className='post' onClick={()=> { navigate('/asdf') }}>메인2</button> */}
-          
+        
           <Button variant="text" className='post' 
           onClick={()=> setModalIsOpen(true)}>
             로그인</Button>
@@ -64,21 +103,22 @@ function Header(props) {
       <Modal isOpen={modalIsOpen} style={modalstyle}>
         <SignBox>
           <SignInBlock>
-            <h1>로그인</h1>
-            <p>아이디</p>
-            <input ref={id_ref}
-              type="text" id="id" placeholder="아이디를 입력하세요" />
-            <p>비밀번호</p>
-            <input ref={pw_ref}
-              type="password" pw="pw" placeholder="비밀번호를 입력하세요" />
-            {/* 1. 시작점 */}
-
-          <Button variant="outlined" onClick={callSomethingAxios}>
-            로그인</Button>
-            <button onClick={() => setModalIsOpen(false)}>닫기</button>
-          </SignInBlock>
-        </SignBox>
-
+          <h1>로그인</h1>
+          
+          <input ref={id_ref} id="id" onChange={(e) => {
+              setId(e.target.value);
+            }} type="text" placeholder="아이디를 입력하세요"/>
+         
+          <input ref={pw_ref} id="pw" type="password" onChange={(e) => {
+              setPwd(e.target.value);
+            }} placeholder="비밀번호를 입력하세요" />
+         
+         <div className='btn'>
+         <Button onClick={()=> setModalIsOpen(false)} variant="outlined">닫기</Button>
+         <Button onClick={callSomethingAxios} variant="outlined">로그인</Button>
+         </div>
+        </SignInBlock>    
+      </SignBox>
       </Modal>
     </>
   )
@@ -125,6 +165,7 @@ padding: 8px 12px;
 
 .post {
   padding-right: 10px;
+  
 }
 
 .signout {
@@ -163,43 +204,48 @@ h1 {
     margin-bottom: 40px;
 }
 
-p {
-    margin: 7px;
-    padding-right: 200px;
-}
-
 input {
     &:hover { 
-        outline: 1.5px solid black;
+        outline: 1.5px solid lightgray;
     }
-    width: 250px;
-    height: 35px;
-    border: 2px solid lightgray;
+    background: #f0f8ff;
+    border: none;
     outline: none;
+    width: 250px;
+    height: 45px;
+    border: none;
+    outline: none;
+    margin-bottom: 10px;
+    border-radius: 8px;
 }
 
-button {
-    width: 100px;
-    margin-top: 30px;
-   
+input::placeholder {
+  padding-left: 8px;
+}
+
+.btn {
+    margin: 0 auto;
+    margin-top: 30px; 
 }
 `;
 
 // Modal 스타일 (기본default값임)
 const modalstyle = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(255, 255, 255, 0.45)",
-    zIndex: 10,
-  },
-  content: {
-    background: "#fafafa",
-    overflow: "auto",
-    top: '40px',
+
+	overlay: {
+		position: "fixed",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: "rgba(255, 255, 255, 0.45)",
+		zIndex: 10,
+	},
+	content: {
+		background: "aliceblue",
+		overflow: "auto",
+		top: '40px',
+
     left: '40px',
     right: '40px',
     bottom: '40px',
